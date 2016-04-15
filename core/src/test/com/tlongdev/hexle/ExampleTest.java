a5b44f46547ba431d6b2e9fec06a95b8f1116883
Long Tran <lngtrn94@gmail.com>
2016-04-15 11:36:41 +0200

    restructre core module for tests

---
+++ core/src/test/com/tlongdev/hexle/ExampleTest.java
@@ -0,0 +1,16 @@
+package com.tlongdev.hexle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author longi
+ * @since 2016.04.15.
+ */
+public class ExampleTest {
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+}
