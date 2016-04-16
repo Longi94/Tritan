7653180b0e0b158ec8396a5fa192ab5e8707ae7b
Long Tran <lngtrn94@gmail.com>
2016-04-16 11:21:39 +0200

    cusomt android config

--- android/src/com/tlongdev/hexle/AndroidLauncher.java
+++ android/src/com/tlongdev/hexle/AndroidLauncher.java
@@ -9,7 +9,7 @@ public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        AndroidApplicationConfiguration config = new HexleAndroidConfiguration();
         initialize(new HexleGame(), config);
     }
 }
