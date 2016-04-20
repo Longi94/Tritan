8837f04fb6036db976f3f3119ed32c3c8cad3c46
Long Tran <lngtrn94@gmail.com>
2016-04-20 21:57:28 +0200

    added crashlytics

--- android/src/com/tlongdev/hexle/AndroidLauncher.java
+++ android/src/com/tlongdev/hexle/AndroidLauncher.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+
+import io.fabric.sdk.android.Fabric;
 
 public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics(), new Answers());
         AndroidApplicationConfiguration config = new HexleAndroidConfiguration();
         initialize(new HexleGame(), config);
     }
